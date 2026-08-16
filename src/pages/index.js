c64558679d21fa13837a45fef62858310ecc3355
Shubham Bisht <bishtshubham906@gmail.com>
2026-08-16 22:05:19 +0530

    export lead CRM dashboard page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as RegisterPage } from "./RegisterPage";
 export { default as DashboardPage } from "./DashboardPage";
 export { default as VendorsPage } from "./VendorsPage";
 export { default as LeadsPage } from "./LeadsPage";
+export { default as LeadDashboardPage } from "./LeadDashboardPage";
